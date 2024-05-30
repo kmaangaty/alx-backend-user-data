b848efbe87ce79389454a201d270fe1407783bbe
Kmangaty <kmaangaty@gmail.com>
2024-05-31 02:37:40 +0300

    setup

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""
+Filtered Logger Module
+"""
+
+import re
+import logging
+from typing import List, Tuple
+import os
+import mysql.connector
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    Obfuscates specified fields in a log message.
+
+    Args:
+        fields (List[str]): Fields to obfuscate.
+        redaction (str): String to replace the field values.
+        message (str): Log message.
+        separator (str): Field separator in the log message.
+
+    Returns:
+        str: Obfuscated log message.
+    """
+    pattern = r'({}=)([^{}]*)'.format('|'.join(fields), separator)
+    return re.sub(pattern, r'\1' + redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats the log record by obfuscating specified fields.
+
+        Args:
+            record (logging.LogRecord): Log record to format.
+
+        Returns:
+            str: Formatted log record.
+        """
+        return filter_datum(self.fields, self.REDACTION, super().format(record), self.SEPARATOR)
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+def get_logger() -> logging.Logger:
+    """
+    Creates and returns a logger for user data.
+
+    Returns:
+        logging.Logger: Configured logger.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Connects to the MySQL database and returns the connection object.
+
+    Returns:
+        mysql.connector.connection.MySQLConnection: Database connection object.
+    """
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    database = os.getenv('PERSONAL_DATA_DB_NAME')
+    return mysql.connector.connect(user=user, password=password, host=host, database=database)
