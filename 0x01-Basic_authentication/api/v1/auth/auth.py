4077352cd2b6612b7727cd97601db0fd31d103a4
Kmangaty <kmaangaty@gmail.com>
2024-06-09 16:17:14 +0300

    setup

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,18 +8,22 @@ from typing import List, TypeVar
 class Auth:
     """ Class to manage the API authentication """
 
-    def require_auth(self, request_path: str, excluded_paths: List[str]) -> bool:
+    def require_auth(self, request_path: str,
+                     excluded_paths: List[str]) -> bool:
         """
         Method to validate if the endpoint requires authentication.
 
         Args:
             request_path (str): The path of the request.
-            excluded_paths (List[str]): A list of paths that do not require authentication.
+            excluded_paths (List[str]): A list of
+             paths that do not require authentication.
 
         Returns:
-            bool: True if authentication is required, False otherwise.
+            bool: True if authentication is required,
+             False otherwise.
         """
-        if request_path is None or excluded_paths is None or not excluded_paths:
+        if (request_path is None or excluded_paths
+                is None or not excluded_paths):
             return True
 
         request_path_length = len(request_path)
@@ -41,7 +45,9 @@ class Auth:
                 if normalized_path == excluded_path:
                     return False
             else:
-                if excluded_path[:-1] == request_path[:excluded_path_length - 1]:
+                if excluded_path[:-1] == request_path[
+                                         :excluded_path_length - 1
+                                         ]:
                     return False
 
         return True
@@ -51,25 +57,28 @@ class Auth:
         Method that handles authorization header extraction.
 
         Args:
-            req (flask.Request, optional): The request object. Defaults to None.
+            req (flask.Request, optional): The
+            request object. Defaults to None.
 
         Returns:
-            str: The value of the Authorization header if present, None otherwise.
+            str: The value of the Authorization
+             header if present, None otherwise.
         """
         if req is None:
             return None
 
         return req.headers.get("Authorization", None)
 
-
     def current_user(self, req=None) -> TypeVar('User'):
         """
         Method to validate the current user.
 
         Args:
-            req (flask.Request, optional): The request object. Defaults to None.
+            req (flask.Request, optional):
+             The request object. Defaults to None.
 
         Returns:
-            TypeVar('User'): The user object if validation is successful, None otherwise.
+            TypeVar('User'): The user object if validation
+            is successful, None otherwise.
         """
         return None
